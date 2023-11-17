702e93542f52d2f63d0f6589f7fd0a6b32bf4c1a
Fajarudin Zakariya <123469989+fajarudinzakariya@users.noreply.github.com>
2023-11-17 20:45:52 +0700

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>final project 3 Hacktiv8</Text>
       <StatusBar style="auto" />
     </View>
   );
